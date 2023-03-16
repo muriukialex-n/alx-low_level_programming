c50a88f28ca02fad03459bc362a7781dcdb0e576
muriukialex-n <muriukialex.n@gmail.com>
2023-03-16 05:49:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return : always zero
+ * return : always 0 (succes)
  */
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
