ca9035edf83f1000a21a81a97d286fb8c1cfd7f3
muriukialex-n <muriukialex.n@gmail.com>
2023-03-22 10:06:53 -0700

    taskfinal

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,13 +2,12 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -25,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid days: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
@@ -34,3 +33,4 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
+
