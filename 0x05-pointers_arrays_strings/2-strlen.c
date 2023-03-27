a39841fbb62dcd9fc0f3021f0f991431c8e09767
muriukialex-n <muriukialex.n@gmail.com>
2023-03-27 05:17:36 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,9 +5,9 @@
  * @str: the string to get the length of
  * Return: the length of @str
  */
-size_t _strlen(const char *str)
+int _strlen(const char *str)
 {
-	size_t length = 0;
+	int _strlen = 0;
 
 	while (*str++)
 		length++;
