24f585838b133659904e226459ea5b24599bb081
muriukialex-n <muriukialex.n@gmail.com>
2023-04-05 06:50:26 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,3 +16,19 @@ int _evaluate(int i, int n)
 	return (-1);
 	return (-1);
 }
+/**
+ * *_sqrt_recursion - evaluate sqrt
+ * *@n: integer
+ * *Return: sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+	{
+		return (_evaluate(i, n));
+	}
+}
