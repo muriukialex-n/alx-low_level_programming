e4e980626f7538b458ff428903d0c3780a4d022a
muriukialex-n <muriukialex.n@gmail.com>
2023-04-09 10:36:56 -0700

    basics

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar - writes the charater c to stdout
+ * @c: the character to print
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (writes(1, &, 1));
+}
