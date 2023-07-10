4764a0af7501bdd1e56b4801fd8a9e0aaee47176
muriukialex-n <muriukialex.n@gmail.com>
2023-07-10 11:14:07 -0700

    welf

--- 0x15-file_io/101-elf_header.c
+++ 0x15-file_io/101-elf_header.c
@@ -1,4 +1,10 @@
-#include "main.h"
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
