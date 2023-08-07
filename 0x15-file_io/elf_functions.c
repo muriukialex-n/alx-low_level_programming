301a968b3aec9e0074ec70cee037ee716950d9f3
muriukialex-n <muriukialex.n@gmail.com>
2023-08-07 15:30:23 +0300

    done advanced

---
+++ 0x15-file_io/elf_functions.c
@@ -0,0 +1,12 @@
+/*
+ * File: elf_functions.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "elf_functions.h"
+
+/* Rest of the functions go here */
